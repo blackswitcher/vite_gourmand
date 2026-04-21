4b4bc8fbfb5025c759e4dc21a9acea0acc39fd38
Mike <m.courbesriviere@orange.fr>
2026-04-21 12:38:13 +0200

    reglage de la BDD

---
+++ README.md
@@ -0,0 +1,28 @@
+# Vite & Gourmand - ECF DWWM
+
+Projet realise dans le cadre de l ECF Developpeur Web et Web Mobile.
+
+## Objectif du projet
+
+Vite & Gourmand est une entreprise de traiteur situee a Bordeaux.
+
+L objectif de l application est de permettre aux visiteurs de consulter les menus proposes, de creer un compte, de commander un menu, puis de suivre leurs commandes.
+
+L application doit aussi permettre aux employes et a l administrateur de gerer les menus, les commandes et les avis clients.
+
+## Stack technique
+
+- PHP
+- MySQL
+- PDO
+- HTML
+- CSS
+- JavaScript
+
+## Base de donnees
+
+La structure de la base de donnees est documentee dans :
+
+```text
+docs/bdd/2026-04-20_vite_gourmand.docx
+docs/bdd/structure_bdd.md
--- src/configs/db.php
+++ src/configs/db.php
@@ -1,7 +1,21 @@
 <?php
 try {
-    $pdo = new PDO('mysql:host=127.0.0.1;port=3307;dbname=vite_gourmand;charset=utf8mb4', 'root', 'root');
+    // connexion PDO a la base mySQL locale
+    //Port 3307
+    // a deplacer en dans un .env
+    $pdo = new PDO(
+        'mysql:host=127.0.0.1;port=3307;dbname=vite_gourmand;charset=utf8mb4', 
+        'root', 
+        'root'
+        );
+
+        // PDO lance une exception en cas d'erreur SQL
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Les resultats SQL seront recuperer sous forme de tableaux associatif 
+
+    $pdo ->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    
 } catch (PDOException $e) {
     die('Erreur : ' . $e->getMessage());
 }
